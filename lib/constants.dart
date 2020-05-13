63877b2ff79b9f9f58d2caf4f3a707a51dd70ca4
Shuaiyin <shuaiyin.top@foxmai..com>
2020-05-13 08:05:12 +0800

    custom Slider

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,9 +4,6 @@ const kBottomContainerHeight = 80.0;
 const kActiveCardColor = Color(0xFF1D1E33);
 const kInactiveCardColor = Color(0xFF111328);
 const kBottomContainerColor = Color(0xFFEB1555);
-const kSliderActiveColor = Color(0xFFEB1555);
-const kSliderInactiveColor = Color(0xFF8D8E98);
-
 
 const labelStyle = TextStyle(
   fontSize: 18.0,
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -90,17 +90,25 @@ class _InputPageState extends State<InputPage> {
                             )
                           ],
                         ),
-                        Slider(
-                          value: height.toDouble(),
-                          min: 120.0,
-                          max: 220.0,
-                          activeColor: kSliderActiveColor,
-                          inactiveColor: kSliderInactiveColor,
-                          onChanged: (double value) {
-                            setState(() {
-                              height = value.floor();
-                            });
-                          },
+                        SliderTheme(
+                          data: SliderTheme.of(context).copyWith(
+                            activeTrackColor: Colors.white,
+                            inactiveTrackColor: Color(0xFF8D8E98),
+                            thumbColor: Color(0xFFEB1555),
+                            overlayColor: Color(0x1fEB1555),
+                            thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0, ),
+                            overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0)
+                          ),
+                          child: Slider(
+                            value: height.toDouble(),
+                            min: 120.0,
+                            max: 220.0,
+                            onChanged: (double value) {
+                              setState(() {
+                                height = value.floor();
+                              });
+                            },
+                          ),
                         )
                       ],
                     ),
